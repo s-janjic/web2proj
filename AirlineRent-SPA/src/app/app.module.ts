4c63ce32ea42ff85055ee5e8dddb3194525a0c14
Stefan <janjicstefan163@gmail.com>
2020-04-11 19:56:07 +0200

    New Components
    + Navigation
    + Home
    + Login
    + Register

--- AirlineRent-SPA/src/app/app.module.ts
+++ AirlineRent-SPA/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
