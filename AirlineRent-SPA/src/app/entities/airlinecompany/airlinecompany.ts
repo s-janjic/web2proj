d0cb0d004d8775802d5f3469b37047c91f10a536
Nemanja Komadina <nemixalone@gmail.com>
2020-04-13 16:11:09 +0200

    Airline addition
    ++ Airline company
    ++ Default vrednosti
    ** Prikaz na home strani

--- AirlineRent-SPA/src/app/entities/airlinecompany/airlinecompany.ts
+++ AirlineRent-SPA/src/app/entities/airlinecompany/airlinecompany.ts
@@ -1,2 +1,3 @@
 export class Airlinecompany {
+    constructor(public typeofair: string,public name:string,public address:string,public description:string,public rating:number){}
 }
--- AirlineRent-SPA/src/app/home/home.component.ts
+++ AirlineRent-SPA/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Rentacar } from '../entities/rentacar/rentacar';
+import { Airlinecompany } from '../entities/airlinecompany/airlinecompany';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,12 @@ import { Rentacar } from '../entities/rentacar/rentacar';
 })
 export class HomeComponent implements OnInit {
   public rentacar: Rentacar = new Rentacar('UNITED Rent-a-Car', 'Bulevar BB', 'Promo description', 4.6);
-
+  public airlines: Airlinecompany[] = [
+    new Airlinecompany('First class','Air Serbia','Nemanjina 14, Beograd, Srbija','First class travel with Serbian company',4.8),
+    new Airlinecompany('First class','Dubai Airline','Shakarid 55b, Dubai, UAE','First class Dubai Airline',4.4),
+    new Airlinecompany('Low cost','RyanAir','Dublin, Ireland','Low cost Irish Air company',4.1),
+    new Airlinecompany('Low cost','Wizz Air','asd 15, Budapest, Hungary','Low cost company from Hungary',4.5),
+  ];
   constructor() { }
 
   ngOnInit(): void {
