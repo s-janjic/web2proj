b18934daa2aa1e77b7eb4039d3a03e8d93d6dac5
Nemanja Komadina <nemixalone@gmail.com>
2020-04-14 18:14:14 +0200

    Default letovi
    ++ random letovi za prikaz

--- AirlineRent-SPA/src/app/entities/airroutes/airroutes.model.ts
+++ AirlineRent-SPA/src/app/entities/airroutes/airroutes.model.ts
@@ -4,14 +4,14 @@ export class AirRoutes{
     public depart: Date;
     public price: number;
     public seats: number;
-    
+    public type: string;
 
-    constructor(from2:string,to2:string,depart2:Date,price2:number,seats2:number){
+    constructor(from2:string,to2:string,depart2:Date,price2:number,seats2:number,type2:string){
         this.from = from2;
         this.to = to2;
         this.depart = depart2;
         this.price = price2;
         this.seats = seats2;
-        
+        this.type = type2;
     }
 }
--- AirlineRent-SPA/src/app/flights/flights.component.ts
+++ AirlineRent-SPA/src/app/flights/flights.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AirRoutes } from '../entities/airroutes/airroutes.model';
 
 @Component({
   selector: 'app-flights',
@@ -7,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlightsComponent implements OnInit {
 
+    datum:Date = new Date(2020,3,24);
+    routes: AirRoutes[] = [
+      new AirRoutes('Belgrade','Dubai',new Date(2020,4,16),250,10,'First Class'),
+      new AirRoutes('Berlin','Los Angeles',new Date(2020,4,16),150,15,'Premium Economy'),
+      new AirRoutes('Belgrade','Berlin',new Date(2020,4,17),35,25,'Economy'),
+      new AirRoutes('London','Belgrade',new Date(2020,4,17),75,40,'Economy'),
+      new AirRoutes('Dubai','Los Angeles',new Date(2020,4,18),250,45,'Premium Economy'),
+      new AirRoutes('Belgrade','Dubai',new Date(2020,4,18),350,55,'First Class'),
+
+    ];
   vrsta: string[] = [
     'Round Trip',
     'One way Trip',
@@ -23,6 +34,8 @@ export class FlightsComponent implements OnInit {
 
   showflights(){
     this.roundtrip = true;
+    console.log(this.routes);
+    console.log(this.datum);
   }
 
   getValueFromTrip(){
